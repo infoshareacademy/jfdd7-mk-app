575e6066f002a5b7c34cf87326d2d96cc3062a0e
Michał Pokojski <michal.pokojski@gmail.com>
2017-07-06 16:27:24 +0200

    Added awesome map which can display any react component on it

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 import Main from './Main'
 import MapSearch from './MapSearch'
 import ListSearch from './ListSearch'
 import Details from './Details'
-
+import Map from './Map'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div style={{width: 800, height: 800}}>
+          <Map>
+          </Map>
+        </div>
         <Main/>
         <hr/>
         <MapSearch/>
@@ -18,6 +22,7 @@ class App extends Component {
         <ListSearch/>
         <hr/>
         <Details/>
+
       </div>
     );
   }
