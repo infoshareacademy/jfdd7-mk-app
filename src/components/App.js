7733a090b3e34afc3867b42905f2976c2e93bfe5
michal.pokojski <michal.pokojski@gmail.com>
2017-07-06 21:49:21 +0200

    added required changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,11 @@ import Main from './Main'
 import MapSearch from './MapSearch'
 import ListSearch from './ListSearch'
 import Details from './Details'
-import Map from './Map'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div style={{width: 800, height: 800}}>
-          <Map>
-          </Map>
-        </div>
         <Main/>
         <hr/>
         <MapSearch/>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,14 @@
  * Created by dominikakosiedowska on 06.07.17.
  */
 import React from 'react'
+import Map from './Map'
+
 const Main  = () => (
   <div>
     Main
+      <div style={{width: 800, height: 800}}>
+          <Map/>
+      </div>
   </div>
 )
 export default Main
