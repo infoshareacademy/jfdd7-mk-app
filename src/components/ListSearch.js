eaf4499294a25c9057a7586bc5ca27985af15859
mkolasinski <m.kolasinski@poczta.fm>
2017-07-06 23:33:04 +0200

    - added bootstrap columns to ListSearch section

--- src/components/ListSearch.js
+++ src/components/ListSearch.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
-import './ListSearch.css';
+import './ListSearch.css'
 
 const ListSearch  = () => (
     <Row>
-      <Col xs={3} className="Pine">To jest jakaś pinezka</Col>
-      <Col xs={9} className="Description">To jest jakiś opis</Col>
+      <Col xs={3} className="Pine">
+        TU MA BYC PINEZKA Z LOGIEM KATEGORII
+      </Col>
+      <Col xs={9}>
+        <div className="Description">
+          TU MA BYC MALY OPIS OBIEKTU (nazwa, odleglosc, nr tel, email...)
+        </div>
+      </Col>
     </Row>
 )
 export default ListSearch
