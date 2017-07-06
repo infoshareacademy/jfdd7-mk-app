b4d410c600da27e0f642fa8a6d27a015ab11c0d2
Dominika Kosiedowska <d.kosiedowska@gmail.com>
2017-07-06 15:51:19 +0200

    szkieletor

---
+++ src/components/Details.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dominikakosiedowska on 06.07.17.
+ */
+import React from 'react'
+const Details = () => (
+  <div>
+    Details
+  </div>
+)
+export default Details
---
+++ src/components/MapSearch.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dominikakosiedowska on 06.07.17.
+ */
+import React from 'react'
+const MapSearch  = () => (
+  <div>
+    MapSearch
+   </div>
+)
+export default MapSearch
