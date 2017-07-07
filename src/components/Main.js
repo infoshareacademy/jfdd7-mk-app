d8e71f94e919d0d123010be5b15a221742727c4e
Michał Pokojski <michal.pokojski@gmail.com>
2017-07-07 17:00:36 +0200

    Próba ostylowania mapy

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,17 @@
 import React from 'react'
 import Map from './Map'
 import SearchField from "./SearchField";
+import './Main.css'
+
 
 const Main = () => (
   <div>
-    <SearchField/>
-    <div style={{width: 1200, height: 800}}>
+    <div className="main-map-style">
       <Map>
         <SearchField/>
       </Map>
     </div>
+
   </div>
 )
 export default Main
