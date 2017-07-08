35f01c06da13378c018f3930fd4856b30292db8b
mkolasinski <m.kolasinski@poczta.fm>
2017-07-08 16:15:58 +0200

    - added section contact - added styles for description - links to website and to telephone

--- src/components/ListSearch.js
+++ src/components/ListSearch.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
 
 import './ListSearch.css'
 
-const ListSearch  = () => (
-    <Row>
-      <Col xs={3} className="pine">
-        <h2> TU JEST PINEZKA KATEGORII </h2>
-      </Col>
-      <Col xs={9}>
-        <div className="description">
-          <h3> Calypso Przymorze <span> ~ odległość: 0,3 km </span></h3>
-          <div className="address">
-            <address className="street"> Adres:<small> ul. Obrońców Wybrzeża 1</small>
-          <p> Telefon: <small> +48 555 555 55 </small></p>
-          <p> Email: <small> calypso.przymorze@calypso.pl </small> </p>
-          </address>
-          </div>
+const ListSearch = () => (
+  <Row>
+    <Col lg={2} className="pine">
+      <h2> TU JEST PINEZKA KATEGORII </h2>
+    </Col>
+    <Col lg={3} className="main-description">
+      <section className="description">
+        <h3> Calypso Przymorze <span className="distance"> ~ odległość: 0,3 km </span></h3>
+          <span> ul. Obrońców Wybrzeża 1 <br/> 80-432 Gdańsk</span>
+        <div className="website">
+          <a href="https://www.calypso.com.pl/klub/calypso-gdansk-przymorze">www.calypso.com.pl/gdansk-przymorze</a>
         </div>
-      </Col>
-    </Row>
+      </section>
+    </Col>
+    <Col lg={7} className="contact">
+      <text>Kontakt:<a href="tel: 0048 555 55 55">+48 555 55 55</a></text>
+    </Col>
+  </Row>
 );
 export default ListSearch
