abf6447dfcec113b5b8561060ecc7469f0fd867a
mkolasinski <m.kolasinski@poczta.fm>
2017-07-08 22:27:38 +0200

    - zmiana tła po najechaniu na div - aktywowanie linków do www obiektu

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,7 @@ import './ListSearch.css'
 
 const Description = () => (
   <section className="description">
-    <h3> Calypso Przymorze <span className="distance"> ~ odległość: 0,3 km </span></h3>
+    <h3><a href="https://www.calypso.com.pl/klub/calypso-gdansk-przymorze">Calypso Przymorze</a> <span className="distance"> ~ odległość: 0,3 km </span></h3>
     <span> ul. Obrońców Wybrzeża 1 <br/> 80-432 Gdańsk</span>
     <div className="website">
       <a href="https://www.calypso.com.pl/klub/calypso-gdansk-przymorze">www.calypso.com.pl/gdansk-przymorze</a>
