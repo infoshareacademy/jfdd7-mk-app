0bac315f668ed5d6fd7c3c8419955e5a0909f623
aircraftman3 <aircraftman3@gmail.com>
2017-07-10 16:54:11 +0200

    funckaj na klasę

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -12,13 +12,52 @@ import {
   from 'react-bootstrap'
 import './Main.css'
 
+export default class SearchField extends React.Component {
+
+  componentWillMount() {
+    fetch(
+      'http://localhost:3000/data/employees.json'
+    ).then(
+      response => response.json()
+    ).then(
+      data => this.setState({
+        employees: data
+      })
+    ).catch(
+      error => console.log(error.message)
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Employees</h1>
+        <ul>
+          { this.state === null ? <p>Fetching data ....</p> : null}
+          {
+            this.state !== null && this.state.employees.map(
+              employee => (
+                <li key={employee.id}>
+                  <Link to={'/employees/' + employee.id}>
+                    {employee.name}
+                  </Link>
+                </li>
+              )
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
 const SearchField = () => (
 
-  <div className="search-field">
+  <div className="search-field container-fluid">
     <Grid>
       <Row>
         <Form>
-          <Col md={7}>
+          <Col md={7} mdOffset={2}>
             <FormGroup controlId="formInlineEmail">
               <ControlLabel>Wyszukaj obiekt</ControlLabel>
               <FormControl type="email" placeholder="Rozwiń listę"/>
