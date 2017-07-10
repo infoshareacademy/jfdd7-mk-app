73faa8e1edbe8f199ae536829198ed7442ddb327
mkolasinski <m.kolasinski@poczta.fm>
2017-07-10 19:57:18 +0200

    - added componentWillMount() and fetch to ListSearch.js

---
+++ src/components/Description.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ListSearch.css'
+
+const Description = () => (
+  <section className="description">
+    <h3><a href="https://www.calypso.com.pl/klub/calypso-gdansk-przymorze">Calypso Przymorze</a> <span className="distance"> ~ odległość: 0,3 km </span></h3>
+    <span> ul. Obrońców Wybrzeża 1 <br/> 80-432 Gdańsk</span>
+    <div className="website">
+      <a href="https://www.calypso.com.pl/klub/calypso-gdansk-przymorze">www.calypso.com.pl/gdansk-przymorze</a>
+    </div>
+  </section>
+)
+export default Description
--- src/components/ListSearch.js
+++ src/components/ListSearch.js
@@ -1,10 +1,59 @@
-/**
- * Created by dominikakosiedowska on 06.07.17.
- */
 import React from 'react'
-const ListSearch  = () => (
-  <div>
-    ListSearch
-  </div>
-)
-export default ListSearch
+import {Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import IconCategory from './IconCategory'
+import Description from './Description'
+import ContactObject from './ContactObject'
+import './ListSearch.css'
+
+export default class ListSearch extends React.Component {
+
+  state = {
+    places: []
+  }
+
+  componentWillMount() {
+    fetch(
+      process.env.PUBLIC_URL + '/data/places.json'
+    ).then(
+      reponse => response.json()
+    ).then(
+      places => this.setState({
+        places
+      })
+    )
+  }
+
+  render() {
+    const placeId = parseInt(this.props.match.params.placeId, 10)
+    const place = this.state.places.find(
+      place => place.id === placeId
+    )
+
+    if (place === undefined) {
+      return (
+        <div>
+          <h1> Not found yet</h1>
+        </div>
+      )
+    }
+
+    return (
+      <Row className="info">
+        <Col xs={12} lg={2} className="pin">
+          <div>
+            <IconCategory/>
+          </div>
+        </Col>
+
+        <Col xs={12} lg={3} className="main-description">
+          <Description/>
+        </Col>
+
+        <Col xs={12} lg={7} className="contact">
+          <ContactObject/>
+        </Col>
+      </Row>
+    )
+  }
+}
