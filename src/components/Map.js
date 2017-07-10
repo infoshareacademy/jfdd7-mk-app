eb9a67bc35f9cffd52818650570295951eb4b1ac
Michał Pokojski <michal.pokojski@gmail.com>
2017-07-10 16:42:38 +0200

    Added example points on map, still need to style them into some nice styled pointers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
-import SearchField from "./SearchField";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({text}) => <div>{text}</div>;
 
 export default class Map extends Component {
+
+
+  state = {
+    places: [],
+  };
+
   static defaultProps = {
     center: {lat: 54.403351, lng: 18.569951},
     zoom: 15
   };
 
+  componentWillMount() {
+    fetch(
+      process.env.PUBLIC_URL + '/places.json'
+    ).then(
+      response => response.json()
+    ).then(
+      places => this.setState({
+        places: places
+      })
+    ).catch(
+      error => console.log(error.message)
+    )
+  }
+
+
   render() {
+
+
     return (
       <GoogleMapReact
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
       >
-        <AnyReactComponent
-          lat={54.403351}
-          lng={18.569951}
-          text={<h45636>Tu jestes, i kiedys to bedzie ikonka , ale jeszcze nie teraz  ;P</h45636>}
-        />
+        {
+          this.state.places.map(
+            place => (
+              <AnyReactComponent
+                lat={parseFloat(place.latitiude)}
+                lng={parseFloat(place.longitude)}
+                text={<h4>{place['place-name']}</h4>}
+              />))
+        }
       </GoogleMapReact>
     );
   }
