c2e9b6a51a5783a51678bbb339fed5b2127b2b8f
Dominika Kosiedowska <d.kosiedowska@gmail.com>
2017-07-10 16:53:50 +0200

    ładowanie danych z bazy

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,24 +1,62 @@
-/**
- * Created by dominikakosiedowska on 06.07.17.
- */
 import React from 'react'
 import {
-    Row,
-    Col
+  Row,
+  Col
 } from'react-bootstrap'
+import {Link} from 'react-router-dom'
 import Foto from './Foto'
 import ObjectName from './ObjectName'
 import ObjectDetails from './ObjectDetails'
-const Details = () => (
-    <Row>
-        <Col xs={12} md={5} mdPush="{7}">
-            <ObjectName/>
-            <ObjectDetails/>
-        </Col>
-        <Col xs={12} md={7} mdPull="{5}">
-            <Foto/>
-        </Col>
-    </Row>
+export default class Details extends React.Component {
 
-)
-export default Details
+  state = {
+    places: []
+  }
+
+  componentWillMount() {
+    fetch(
+      process.env.PUBLIC_URL + '/data/places.json'
+    ).then(
+      response => response.json()
+    ).then(
+      places => this.setState({
+        places
+      })
+    )
+  }
+
+
+  render() {
+    const placeId = parseInt(this.props.match.params.placeId, 10)
+    const place = this.state.places.find(
+      place => place.id === placeId
+    )
+
+    if (place === undefined) {
+      return (
+        <div>
+          <h1>Not found yet...</h1>
+        </div>
+      )
+    }
+
+    return (
+      <Row>
+        <Col xs={12} md={5} mdPush={7}>
+          <ObjectName name={place.name}/>
+          <Link to={'/places/' + place.id}>
+            {place.name}
+          </Link>
+          <ObjectDetails address={place.adress} telephone={place.telephone} mail={place.mail} website={place.website}/>
+        </Col>
+        <Col xs={12} md={7} mdPull={5}>
+          <Foto/>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+
+
+
--- src/components/ObjectDetails.js
+++ src/components/ObjectDetails.js
@@ -2,12 +2,14 @@
  * Created by dominikakosiedowska on 06.07.17.
  */
 import React from 'react'
-const ObjectDetails = () => (
+
+const ObjectDetails = ({address, telephone, mail, website}) => (
   <section>
-      <h5>Adres:</h5>
-      <h5>Telefon:</h5>
-      <h5>E-mail:</h5>
-      <h5>Witryna:</h5>
+    <h5>Adres: {address}</h5>
+    <h5>Telefon: {telephone}</h5>
+    <h5>E-mail: {mail}</h5>
+    <h5>Witryna: {website}</h5>
   </section>
-)
+  )
+
 export default ObjectDetails
