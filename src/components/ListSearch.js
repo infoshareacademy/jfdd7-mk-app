221725b2d7bd31eb08a8e1799df4b52321210cac
mkolasinski <mkolasinski@poczta.fm>
2017-07-11 16:13:06 +0200

    - zmiany szerokosci glownego div all-description

--- src/components/ListSearch.js
+++ src/components/ListSearch.js
@@ -27,29 +27,30 @@ export default class ListSearch extends React.Component {
   render() {
     console.log(this.state)
     return (
-      <div>
+      <div className="all-description">
+
         {
           this.state.places === null ?
             null :
             this.state.places.map(
               place => (
                 <Link to={'/places/' + place.id}>
-                <Row className="info">
-                  <Col xs={12} lg={2} className="pin">
-                    <div>
-                      <IconCategory/>
-                    </div>
-                  </Col>
+                  <Row className="info">
+                    <Col xs={12} lg={2} className="pin">
+                      <div>
+                        <IconCategory/>
+                      </div>
+                    </Col>
 
-                  <Col xs={12} lg={3} className="main-description">
-                    <Description address={place.address} telephone={place.telephone} website={place.website}
-                                 name={place.name}/>
-                  </Col>
+                    <Col xs={12} lg={3} className="main-description">
+                      <Description address={place.address} telephone={place.telephone} website={place.website}
+                                   name={place.name}/>
+                    </Col>
 
-                  <Col xs={12} lg={7} className="contact">
-                    <ContactObject telephone={place.telephone}/>
-                  </Col>
-                </Row>
+                    <Col xs={12} lg={7} className="contact">
+                      <ContactObject telephone={place.telephone}/>
+                    </Col>
+                  </Row>
                 </Link>
               )
             )
