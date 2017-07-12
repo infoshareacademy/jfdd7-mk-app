0fb675437e4aee6d4fb610ad0da8b7be951ea7c1
Dominika Kosiedowska <d.kosiedowska@gmail.com>
2017-07-12 15:53:45 +0200

    dodanie React-reduxa i reduxa i thunka

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+
+import store from './store'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
+
+import './index.css'
+
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+  <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
