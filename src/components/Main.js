fc6570d00a4ca5b28a9dadd0c37f531a644cf7ec
aircraftman3 <aircraftman3@gmail.com>
2017-07-13 02:50:29 +0200

    wip

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,14 @@
-/**
- * Created by dominikakosiedowska on 06.07.17.
- */
 import React from 'react'
 import Map from './Map'
+import SearchField from "./SearchField";
 import './Main.css'
 
+const Main  = () => (
+  <div className="div-map">
+    <SearchField/>
 
-const Main = () => (
-  <div>
-    <div className="main-map-style">
-      <Map>
-      </Map>
+    <div className="center-block" style={{width: 800, height: 800}}>
+      <Map/>
     </div>
   </div>
 )
