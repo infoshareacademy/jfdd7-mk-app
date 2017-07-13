c5e3e5e821726e96672c3f542325366ccecea1ec
mkolasinski <mkolasinski@poczta.fm>
2017-07-13 15:23:56 +0200

    - mapa z Main do MapSearch - utworzenie MapSearch.css - utworzenie Main.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,12 @@
-/**
- * Created by dominikakosiedowska on 06.07.17.
- */
 import React from 'react'
-import Map from './Map'
+// import Map from './Map'
 import './Main.css'
 
 
 const Main = () => (
-  <div>
-    <div className="main-map-style">
-      <Map>
-      </Map>
-    </div>
+  <div className="background">
+
   </div>
+
 )
 export default Main
