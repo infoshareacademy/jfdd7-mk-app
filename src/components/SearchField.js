b4217a31f9e72983ccad1ab93b85d340f117c70a
Michal Pokojski <michal.pokojski@gmail.com>
2017-07-16 20:30:54 +0200

    Przycisk wyszukaj przekierowuje do listsearch

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,38 +1,41 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { FormControl, Grid, Row, Col, FormGroup, Button } from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {FormControl, Grid, Row, Col, FormGroup, Button} from 'react-bootstrap'
 import './SearchField.css'
+import {LinkContainer} from 'react-router-bootstrap'
 
-import { updateSearchPhrase } from '../state/searchField'
+import {updateSearchPhrase} from '../state/searchField'
 
 const SearchField = props => (
-<Grid>
-  <Row>
-  <Col sm={6}>
-    <FormGroup controlId="formInlineEmail">
-    <FormControl
-      value={props.searchPhrase}
-      onChange={props.update}
-      placeholder="Wyszukaj..."
-    />
-    </FormGroup>
-  </Col>
-    <Col sm={6}>
-      <Button type="submit"
-              bsStyle="info"
-              bsSize="large"
-              >
-        Wyszukaj
-      </Button>
-      <Button type="submit"
-              bsStyle="info"
-              bsSize="large"
-              >
-        Filtruj
-      </Button>
-    </Col>
-  </Row>
-</Grid>
+  <Grid>
+    <Row>
+      <Col sm={6}>
+        <FormGroup controlId="formInlineEmail">
+          <FormControl
+            value={props.searchPhrase}
+            onChange={props.update}
+            placeholder="Wyszukaj..."
+          />
+        </FormGroup>
+      </Col>
+      <Col sm={6}>
+        <LinkContainer to="/list-search">
+          <Button type="submit"
+                  bsStyle="info"
+                  bsSize="large"
+          >
+            Wyszukaj
+          </Button>
+        </LinkContainer>
+        <Button type="submit"
+                bsStyle="info"
+                bsSize="large"
+        >
+          Filtruj
+        </Button>
+      </Col>
+    </Row>
+  </Grid>
 );
 
 export default connect(
