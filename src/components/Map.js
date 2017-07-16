56e5a6be698906c80f2dee4c064eab1e9b5b9ad0
Michal Pokojski <michal.pokojski@gmail.com>
2017-07-16 13:18:14 +0200

    markers positioning works perfect right now

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,48 +6,67 @@ import {fetchPlaces} from '../state/places'
 
 export default connect(
   state => ({
-    places: state.places
+    places: state.places,
+    searchPhrase: state.searchField.searchPhrase
   }),
   dispatch => ({
     fetchPlaces: () => dispatch(fetchPlaces())
   })
-) (
+)(
   class Map extends Component {
 
-static defaultProps = {
-    center: {lat: 54.403351, lng: 18.569951},
-    zoom: 15
-  };
+    static defaultProps = {
+      center: {lat: 54.403351, lng: 18.569951},
+      zoom: 1
+    };
 
-  componentWillMount() {
-    this.props.fetchPlaces()
-  }
+    componentWillMount() {
+      this.props.fetchPlaces()
+    }
 
 
-  render() {
-    const {data} = this.props.places
-    return (
-      <GoogleMapReact
-        apiKey={'AIzaSyAJcR-ZM6KPN20sN1ECp89Jgi0hqJvQBho'}
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
+    render() {
+      const {data} = this.props.places
+      const checkString = string => string.toLowerCase().includes(this.props.searchPhrase.toLowerCase())
+      const checkArray = functions => this.props.searchPhrase.toLowerCase().split(' ').every(phrase => functions.join(' ').toLowerCase().includes(phrase))
+      const placeStyle = {
+        width: 35,
+        height: 35,
+        backgroundImage: `url(${marker})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        position: 'absolute',
+        top: -35,
+        left: -35/2
 
-        {data !== null && data.map(
-          place => (
-            <div
-              style={{width: 35, height: 35}}
-              lat={parseFloat(place.latitude)}
-              lng={parseFloat(place.longitude)}
-            >
-              <img
-                src={marker}
-                alt={place.name}
-              />
-            </div>
-          ))}
+      }
 
-      </GoogleMapReact>
-    );
-  }
-})
+
+      return (
+        <GoogleMapReact
+          apiKey={'AIzaSyAJcR-ZM6KPN20sN1ECp89Jgi0hqJvQBho'}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+
+          {data !== null && data.filter(
+            place => this.props.searchPhrase === '' ? false : checkString(place.name) || checkArray(place.functions)
+          ).map(
+            place => (
+              <div
+                style={placeStyle}
+                lat={parseFloat(place.latitude)}
+                lng={parseFloat(place.longitude)}
+
+              >
+                {/*<img*/}
+                {/*src={marker}*/}
+                {/*alt={place.name}*/}
+                {/*/>*/}
+              </div>
+            ))}
+
+        </GoogleMapReact>
+      );
+    }
+  })
--- src/components/MapSearch.js
+++ src/components/MapSearch.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Map from './Map'
+import SearchField from './SearchField'
 
 const MapSearch  = () => (
   <div>
+    <SearchField/>
     <div style={{ height: "800px"}}>
       <div className="center-block"
            style={{width: "80%", height: "100%"}}
