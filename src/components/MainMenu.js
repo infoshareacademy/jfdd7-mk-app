5be93c3a9854bbf29919439e5d1b7d8d6d024825
Dominika Kosiedowska <d.kosiedowska@gmail.com>
2017-07-30 19:55:02 +0200

    border-bottom on main menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import {
   LinkContainer,
@@ -12,45 +12,50 @@ import {
 import firebase from 'firebase'
 import {connect} from 'react-redux'
 
-const MainMenu = ({user}) => (
-  <Navbar collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link className='Main__Menu' to="/">Gdzie poćwiczę?</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <IndexLinkContainer to="/map-search">
-          <NavItem className='Main__Menu'>MAPA</NavItem>
-        </IndexLinkContainer>
-        <LinkContainer to="/list-search">
-          <NavItem className='Main__Menu'>LISTA</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/main">
-          <NavItem className='Main__Menu'>LOGOWANIE</NavItem>
-        </LinkContainer>
-        {user === null ? null :
-          <LinkContainer to="/favorites">
-            <NavItem className='Main__Menu'>ULUBIONE</NavItem>
-          </LinkContainer>
-        }
-      </Nav>
-      <Nav pullRight>
-        {user === null ? null :
-          <LinkContainer to="/list-search">
-            <NavItem className="signOutButton"
-                     onClick={() => firebase.auth().signOut()}>
-              Wyloguj się
-            </NavItem>
-          </LinkContainer>
+class MainMenu extends Component {
+  render() {
+    const user = this.props.user;
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link className='Main__Menu' to="/">Gdzie poćwiczę?</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <IndexLinkContainer to="/map-search">
+              <NavItem className='Main__Menu'>MAPA</NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to="/list-search">
+              <NavItem className='Main__Menu'>LISTA</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/logowanie">
+              <NavItem className='Main__Menu'>LOGOWANIE</NavItem>
+            </LinkContainer>
+            {user === null ? null :
+              <LinkContainer to="/favorites">
+                <NavItem className='Main__Menu'>ULUBIONE</NavItem>
+              </LinkContainer>
+            }
+          </Nav>
+          <Nav pullRight>
+            {user === null ? null :
+              <LinkContainer to="/list-search">
+                <NavItem className="signOutButton"
+                         onClick={() => firebase.auth().signOut()}>
+                  Wyloguj się
+                </NavItem>
+              </LinkContainer>
 
-        }
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
 
 export default connect(
   state => ({
